481d3a4ba41fe7bd7edbf6d8c42283d2021ab101
Aneeq Iftikhar <kh.aneeq@gmail.com>
2024-08-04 06:57:53 +0500

    Adding Sample Test

--- catalog_service/src/services/__test__/catalogService.test.ts
+++ catalog_service/src/services/__test__/catalogService.test.ts
@@ -3,4 +3,8 @@ describe("catalogService", () => {
     const a = 10;
     expect(a).toEqual(10);
   });
+  test("Example Test 2", () => {
+    const a = "Hello";
+    expect(a).toEqual(a)
+  });
 });
